1dc884f0ef88352a1b2edfddb5967928fb84768f
DIFoundation <91672319+DIFoundation@users.noreply.github.com>
2024-08-03 12:50:47 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7129018346117539"
+     crossorigin="anonymous"></script>
+      </head>
     </html>
   );
 }
